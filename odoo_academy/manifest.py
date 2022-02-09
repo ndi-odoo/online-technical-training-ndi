6dc960d733a68b46c61d41381aac5b621e155f31
ndi-odoo <ndi@odoo.com>
2022-02-09 19:30:29 +0000

    esqueleto del proyecto

---
+++ odoo_academy/manifest.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Odoo Academy',
+    
+    'summary': """Academy app to manage Training""",
+    
+    'description': """
+        Academy module to manage training
+        -courses
+        -sessiones
+        -attendees""",
+    'author': "Odoo",
+    'website': 'http..',
+    'category': 'Training',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        
+    ],
+    'license':'LGPL-3',
+    'demo': [
+       
+    ]
+    
+}
