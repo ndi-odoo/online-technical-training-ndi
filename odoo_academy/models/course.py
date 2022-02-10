334d7c87958d4fd58853b8adc11865f6b38c22e0
ndi-odoo <ndi@odoo.com>
2022-02-10 06:07:54 +0000

    corrige  comillas

--- odoo_academy/models/course.py
+++ odoo_academy/models/course.py
@@ -9,5 +9,5 @@ class Course(models.Model):
     name = fields.Char(string='Title',required=True)
     description = fields.Text(string='Description')
     level = fields.Selection(string='Level',
-                            selection=[('beginner,Beginner'),('intermediate,Intermediate'),('advanced,Advanced')],
+                            selection=[('beginner','Beginner'),('intermediate','Intermediate'),('advanced','Advanced')],
                             copy=False)
