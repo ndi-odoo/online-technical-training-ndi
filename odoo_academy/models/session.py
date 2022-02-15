7acd8b3c83265895e67b00c2d22d181daaba9904
ndi-odoo <ndi@odoo.com>
2022-02-15 20:44:12 +0000

    advanced views

--- odoo_academy/models/session.py
+++ odoo_academy/models/session.py
@@ -2,6 +2,7 @@
 
 from odoo import models ,fields, api
 from odoo.exceptions import UserError, ValidationError
+from datetime import timedelta
 
 class Session(models.Model):
      _name = 'academy.session'
@@ -18,5 +19,31 @@ class Session(models.Model):
      instructor_id=fields.Many2one(comodel_name='res.partner',
                                    string='Instructor')
                                    
-     student_ids =fields.Many2many(comodel_name='res.partner',
-                                    string='Students')    
+     student_ids = fields.Many2many(comodel_name='res.partner',
+                                    string='Students')   
+                                    
+     start_date = fields.Date(string='Start Date',
+                      default=fields.Date.today)
+                      
+     duration = fields.Integer(string='Session days',
+                      default=1)                 
+                                    
+     end_date = fields.Date(string='End Date',
+                            compute='_compute_end_date',
+                            inverse='_inverse_end_date',
+                            store=True)
+                                   
+    @api.depends('start_date','duration')
+    def _compute_end_date(self):
+        for record in self:
+            if not(record.start_date and record.duration):
+                 record.end.date = recorde.start_date
+            else:
+               duration = tiemedelta(days=record.duration)
+               record.end_date=record.start_date + duration
+               
+    def _inverse_end_date(self):
+        for record in self:
+            if record.start_date and record.end_date:
+                record.duration = (record.end_date - record.start_date).days +1                                                
+                                    
