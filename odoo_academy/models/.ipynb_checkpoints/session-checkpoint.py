c0b9617476bb72231fd3a81272dbd359b636b0e2
ndi-odoo <ndi@odoo.com>
2022-02-16 17:41:18 +0000

    indentation correction

--- odoo_academy/models/.ipynb_checkpoints/session-checkpoint.py
+++ odoo_academy/models/.ipynb_checkpoints/session-checkpoint.py
@@ -5,30 +5,30 @@ from odoo.exceptions import UserError, ValidationError
 from datetime import timedelta
 
 class Session(models.Model):
-     _name = 'academy.session'
-     _description = 'Session Info'
+    _name = 'academy.session'
+    _description = 'Session Info'
      
-     course_id = fields.Many2one(comodel_name='academy.course',
+    course_id = fields.Many2one(comodel_name='academy.course',
                                  string='Course',
                                  ondelete='cascade',
                                  required=True)
                                  
-     name= fields.Char(string='Title',
+    name= fields.Char(string='Title',
                        related='course_id.name')
                        
-     instructor_id=fields.Many2one(comodel_name='res.partner',
+    instructor_id=fields.Many2one(comodel_name='res.partner',
                                    string='Instructor')
                                    
-     student_ids = fields.Many2many(comodel_name='res.partner',
+    student_ids = fields.Many2many(comodel_name='res.partner',
                                     string='Students')   
                                     
-     start_date = fields.Date(string='Start Date',
+    start_date = fields.Date(string='Start Date',
                       default=fields.Date.today)
                       
-     duration = fields.Integer(string='Session days',
+    duration = fields.Integer(string='Session days',
                       default=1)                 
                                     
-     end_date = fields.Date(string='End Date',
+    end_date = fields.Date(string='End Date',
                             compute='_compute_end_date',
                             inverse='_inverse_end_date',
                             store=True)
@@ -45,5 +45,5 @@ class Session(models.Model):
     def _inverse_end_date(self):
         for record in self:
             if record.start_date and record.end_date:
-                record.duration = (record.end_date - record.start_date).days +1                                                
+                record.duration = (record.end_date - record.start_date).days+1                                                
                                     
