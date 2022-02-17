1971f21147bd1e5bdb441ccb9c406903daffd572
ndi-odoo <ndi@odoo.com>
2022-02-17 20:55:10 +0000

    wizard

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -20,7 +20,10 @@
         'security/ir.model.access.csv',
         'views/academy_menuitems.xml',
         'views/course_views.xml',
-        'views/session_views.xml'
+        'views/session_views.xml',
+        'views/sale_views_inherit.xml',
+        'views/product_views_inherit.xml',
+        'wizard/sale_wizard_view.xml',
     ],
     'demo': [   
         'demo/academy_demo.xml'
